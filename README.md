66e49cabe9055046361169cff70cb6c6129496d8
Adliano Alves <adliano@me.com>
2019-05-20 22:59:47 -0700

    start server.js and install npm express

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ FriendFinder
 
 A dating application. This full-stack site will take in results from users' surveys, then compare their answers with those from other users.
 The app will then display the name and picture of the user with the best overall match.
+
+# npm install nodemon
+
+https://www.npmjs.com/package/nodemon
+
+# npm install express
---
+++ server.js
@@ -0,0 +1,33 @@
+// instanciate express
+const express = require("express");
+let app = express();
+// init path nodejs module
+const path = require("path");
+// Default PORT used by listener for routing
+const PORT = process.env.PORT || 3000;
+
+// Midlayers to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/**
+ *
+ * GETs Routes
+ *
+ */
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
+
+/**
+ *
+ * SERVER LISTEN
+ *
+ */
+app.listen(PORT, function() {
+  console.log(`Server runnig on PORT ${PORT}`);
+});
