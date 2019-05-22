75403631c90b163aa802861d0261719a4a444b59
Adliano Alves <adliano@me.com>
2019-05-22 00:02:46 -0700

    create the friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,15 @@
-{
-    "name": "adriano",
-    "picture": "wwww.com",
-    "score": [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "5",
-        "4",
-        "3",
-        "2",
-        "1"
-    ]
-}
+/**
+ *
+ * This will hold all data in an array of JSON
+ *
+ */
+
+let friends = [
+  {
+    name: "adriano",
+    picture: "wwww.com",
+    score: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+  }
+];
+
+module.exports = friends;
