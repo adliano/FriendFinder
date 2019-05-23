51d97624b53733a2e9dffb1d480a9259128e512e
Adliano Alves <adliano@me.com>
2019-05-22 21:20:35 -0700

    Calculation part its done, now need to find match

--- server.js
+++ server.js
@@ -14,6 +14,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 /**
+ * Method findMatch()
+ * @param {JSON} currentUsrObj
+ * @returns Promise
+ *
+ * This Method will compares users scores and find the closest match
+ */
+function findMatch(currentUsrObj) {
+  // Get Array with score ofcurrent user
+  let _currentUsrScores = currentUsrObj.score;
+  // Return a Promise
+  return new Promise((resolve, reject) => {
+    //
+    // Clear console for better debug
+    console.clear();
+    // Loop throught friends array to calculate results
+    friends.forEach(function(item) {
+      let _results = [];
+      //console.log(item.score);
+      item.score.forEach(function(item, index) {
+        _results.push(Math.abs(item - _currentUsrScores[index]));
+      });
+      console.log(_results.reduce((a, b) => parseInt(a) + parseInt(b)));
+    });
+    //
+    resolve("user matched");
+  });
+}
+
+/**
  *
  * GETs Routes
  *
@@ -33,16 +62,15 @@ app.get("/survey", function(req, res) {
  *
  */
 app.post("/api/survey", function(req, res) {
-  console.log(req.body);
-  //   fs.appendFile(
-  //     path.join(__dirname, "./app/data/friends.js"),
-  friends.push(JSON.stringify(req.body, null, 4));
-  //     err => {
-  //       if (err) throw err;
+  findMatch(req.body)
+    .then(user => {
+      console.log(`inside then and user = ${user}`);
+    })
+    .then(() => friends.push(req.body));
 
+  //////////
   res.json({ ok: true });
-  //     }
-  //   );
+  //console.log(friends);
 });
 
 /**
@@ -52,6 +80,11 @@ app.post("/api/survey", function(req, res) {
  */
 app.listen(PORT, function() {
   console.log(`Server runnig on PORT ${PORT}`);
-
-  console.log(friends);
 });
+
+//
+/** Notes
+ * fruits.forEach(function(item, index, array) {
+  console.log(item, index);
+});
+ */
