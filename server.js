54f6c5cff50d5dc24f40b6f7adc91fad2c696adb
Adliano Alves <adliano@me.com>
2019-05-25 23:10:54 -0700

    add the modal for error and routes to apiRoutes.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // use fylesystem
-// let fs = require("fs");
-let friends = require("./app/data/friends");
+// let friends = require("./app/data/friends");
 // instanciate express
 const express = require("express");
 let app = express();
@@ -13,47 +12,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/**
- * Method findMatch()
- * @param {Array} usrScore
- * @returns Promise
- *
- * This Method will compares users scores and find the closest match
- */
-function findMatch(usrScore) {
-  // Return a Promise
-  return new Promise((resolve, reject) => {
-    // Max possible score will be 40
-    let _matchedUser = { score: 40 };
-    //
-    // Clear console for better debug
-    console.clear();
-    // Loop throught friends array to calculate results
-    friends.forEach(function(item, usrIndex) {
-      let _results = 0;
+require("./app/routing/apiRoutes")(app);
 
-      //console.log(item.score);
-      // Loop through score array
-      item.score.forEach(function(item, index) {
-        // Get Sum of Diferrence between current user score and saved user score
-        _results = _results + Math.abs(item - usrScore[index]);
-      });
-      // Check if score its close to current user and get its index
-      if (_matchedUser.score > _results) {
-        _matchedUser.score = _results;
-        _matchedUser.index = usrIndex;
-      }
-    });
-    // Pass the Mathed user
-    resolve(friends[_matchedUser.index]);
-  });
-}
-
-/**
+/**************************************
  *
  * GETs Routes
  *
- */
+ **************************************/
 // home
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./app/public/home.html"));
@@ -62,38 +27,31 @@ app.get("/", function(req, res) {
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 });
-// API Friends
-app.get("/api/friends", function(req, res) {
-  res.send(friends);
-});
+// // API Friends
+// app.get("/api/friends", function(req, res) {
+//   res.send(friends);
+// });
 
-/**
+/**************************************
  *
  * POSTs Routes
  *
- */
-app.post("/api/survey", function(req, res) {
-  // Call method findMath() to find Matched user
-  findMatch(req.body.score).then(user => {
-    // then send the matched user to client
-    res.json(user);
-    // and add it to friends array
-    friends.push(req.body);
-  });
-});
+ **************************************/
+// app.post("/api/survey", function(req, res) {
+//   // Call method findMath() to find Matched user
+//   findMatch(req.body.score).then(user => {
+//     // then send the matched user to client
+//     res.json(user);
+//     // and add it to friends array
+//     friends.push(req.body);
+//   });
+// });
 
-/**
+/**************************************
  *
  * SERVER LISTEN
  *
- */
+ **************************************/
 app.listen(PORT, function() {
   console.log(`Server runnig on PORT ${PORT}`);
 });
-
-//
-/** Notes
- * fruits.forEach(function(item, index, array) {
-  console.log(item, index);
-});
- */
