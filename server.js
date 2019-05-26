6649764b8e777d45199c3efcffd1f48347eee9d3
Adliano Alves <adliano@me.com>
2019-05-25 21:31:18 -0700

    add /api/friends route and some comments

--- server.js
+++ server.js
@@ -62,6 +62,10 @@ app.get("/", function(req, res) {
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 });
+// API Friends
+app.get("/api/friends", function(req, res) {
+  res.send(friends);
+});
 
 /**
  *
